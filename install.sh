db8dc0ff8febd7082f7e8f78d9eab4fc9aba9c7b
Dzhekson6000 <dzhekson6000@gmail.com>
2016-08-29 16:10:47 +0300

    fixed install

--- install.sh
+++ install.sh
@@ -6,13 +6,6 @@ if [[ "$(whoami)" != "root" ]]; then
 	exit;
 fi
 
-#если нужно подключится к интернету.
-echo "Нужно подключится по Wifi?(Y/n)"
-read YN
-if ["$YN" != "n" || "$YN" != "N"]; then
-	wifi-menu
-fi
-
 # Проверяем, что интернет есть
 errorscount="$(ping -c 3 google.com 2<&1| grep -icE 'unknown|expired|unreachable|time out')"
 if ["$errorscount" != 0]; then
@@ -20,7 +13,7 @@ if ["$errorscount" != 0]; then
 	exit;
 fi
 
-#Получаем необходимые параметры
+#--------Получаем необходимые параметры--------------------
 echo "Введите имя пользователя которого хотите создать: "
 # Считываем имя пользователя
 read user
@@ -39,8 +32,8 @@ pacman -Syyu
 #Установка iw, зашифрованное соединение, прошифки для вайфая, гит, базовый пакет разработчика, регулятор громкости alsa, терминал, шрифт1, шрифт2,
 #просмотрщик картинок, браузер, Flash-плагин для браузера, росмотрщик изображений, видео плеер,
 #скриншоттер, LightDM, менеджер сетей, виджет раскладки, файловый менеджер
-#диспечер задач, офисный пакет, русский язык для офисного пакета, i3status
-pacman -S iw wpa_supplicant dialog git base-devel alsa-utils rxvt-unicode ttf-droid ttf-dejavu feh firefox flashplugin sxiv vlc shutter Lightdm network-manager-applet sbxkb ranger htop libreoffice-fresh libreoffice-fresh-ru i3status --noconfirm
+#диспечер задач, офисный пакет, русский язык для офисного пакета, i3status, трансляции
+pacman -S iw wpa_supplicant dialog git base-devel alsa-utils rxvt-unicode ttf-droid ttf-dejavu feh firefox flashplugin sxiv vlc shutter Lightdm network-manager-applet sbxkb ranger htop libreoffice-fresh libreoffice-fresh-ru i3status obs-studio --noconfirm
 
 #устанавиваем иксы
 pacman -S xorg-xrandr --noconfirm
@@ -48,8 +41,8 @@ pacman -S xorg-xrandr --noconfirm
 #устанавливаем yaourt
 mkdir /tmp && cd /tmp && git clone https://aur.archlinux.org/yaourt.git && su -c 'cd /tmp/yaourt && makepkg -sri' $user
 
-#Устанавивание пакеты из AUR: оконный менеджер, запуск приложений, редактор видео, текстовый редактор, торрент-качалка
-su -c 'yaourt -S i3-gaps j4-dmenu-desktop-git flowblade atom-editor rtorrent-color --noconfirm' $user
+#Устанавивание пакеты из AUR: оконный менеджер, запуск приложений, редактор видео, текстовый редактор, торрент-качалка, Slack клиент, Telegram клиент
+su -c 'yaourt -S i3-gaps j4-dmenu-desktop-git flowblade atom-editor rtorrent-color slack-desktop telegram-desktop-bin --noconfirm' $user
 
 # Заканчиваем выполнение
 echo -e "Готово! Можно перезагружать компьютер.\n© Ilya Chizanov, 2016"
